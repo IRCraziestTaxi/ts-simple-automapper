beafa90ab23ac8fb706763b3dac44685c63cdce8
Who Cares <it@doesnt.matter>
2019-03-27 23:05:55 -0500

    Implemented build and dist files.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { Hide } from "./decorators/Hide";
+export { Ignore } from "./decorators/Ignore";
+export { MapFrom } from "./decorators/MapFrom";
+export { MapProp } from "./decorators/MapProp";
+export { MapFromOptions } from "./interfaces/MapFromOptions";
+export { PropMappingRule } from "./interfaces/PropMappingRule";
+export { Mapper } from "./mapper/Mapper";
